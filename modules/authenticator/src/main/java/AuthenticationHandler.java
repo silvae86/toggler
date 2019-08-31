2efac321a20321462f660c152c3a170d356b82c3
João Rocha <joaorosilva@gmail.com>
2019-09-01 00:36:42 +0100

    Adding new module to manage Kafka Authentication

---
+++ modules/authenticator/src/main/java/AuthenticationHandler.java
@@ -0,0 +1,44 @@
+import models.auth.APIToken;
+import models.roles.User;
+import org.apache.kafka.common.KafkaException;
+import org.apache.kafka.common.security.auth.AuthenticateCallbackHandler;
+import org.apache.kafka.common.security.plain.PlainAuthenticateCallback;
+
+import javax.security.auth.callback.Callback;
+import javax.security.auth.callback.NameCallback;
+import javax.security.auth.callback.UnsupportedCallbackException;
+import javax.security.auth.login.AppConfigurationEntry;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+public class AuthenticationHandler implements AuthenticateCallbackHandler {
+    private List<AppConfigurationEntry> jaasConfigEntries;
+
+    public void configure(Map<String, ?> configs, String mechanism, List<AppConfigurationEntry> jaasConfigEntries) {
+        this.jaasConfigEntries = jaasConfigEntries;
+    }
+
+    public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
+        String username = null;
+        for (Callback callback : callbacks) {
+            if (callback instanceof NameCallback)
+                username = ((NameCallback) callback).getDefaultName();
+            else if (callback instanceof PlainAuthenticateCallback) {
+                PlainAuthenticateCallback plainCallback = (PlainAuthenticateCallback) callback;
+                boolean authenticated = authenticate(username, plainCallback.password());
+                plainCallback.authenticated(authenticated);
+            } else
+                throw new UnsupportedCallbackException(callback);
+        }
+    }
+
+    protected boolean authenticate(String username, char[] password) throws IOException {
+        APIToken token = User.auth(username, new String(password));
+
+        return token != null;
+    }
+
+    public void close() throws KafkaException {
+    }
+}
